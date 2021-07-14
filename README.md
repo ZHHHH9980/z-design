884bb31e997d16f894facc581e30c0bd4d0e8deb
ZHHHH9980 <773739586@qq.com>
2021-07-14 08:18:17 +0800

    delete default stories

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+# z-design
+
+a React UI component library
+
 ## install
+
+```
+npm i zdesign --save
+```
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,3 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-library.add(fab, fas);
 import { Button } from "./components";
 import { Alert } from "./components";
 import { Menu } from "./components";
@@ -16,4 +12,8 @@ import { AutoComplete } from "./components";
 import { Upload } from "./components";
 import { List } from "./components";
 import { ListItem } from "./components";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fab, fas);
 export { Button, Alert, Menu, SubMenu, MenuItem, Tabs, TabsItem, Icon, Transition, Input, AutoComplete, Upload, List, ListItem, };
